1e4225cfed38fc1a0d63be0a3261dc1001402b3e
Chris Bordeman <cbordeman@outlook.com>
2024-04-27 12:17:01 -0400

    Initial

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+mod camera;
+mod debug;
+mod movement;
+mod spaceship;
+
+use ::bevy::prelude::*;
+use camera::CameraPlugin;
+//use debug::DebugPlugin;
+use movement::MovementPlugin;
+use spaceship::SpaceshipPlugin;
+
+fn main() {
+    App::new()
+        // Bevy
+        .insert_resource(ClearColor(Color::rgb(0.1, 0.0, 0.15)))
+        .insert_resource(AmbientLight {
+            color: Color::default(),
+            brightness: 750.0,
+        })
+        .add_plugins(DefaultPlugins)
+        // User Configured plugins
+        .add_plugins(SpaceshipPlugin)
+        .add_plugins(CameraPlugin)
+        .add_plugins(MovementPlugin)
+        //.add_plugins(DebugPlugin)
+        .run();
+}
---
+++ src/movement.rs
@@ -0,0 +1,37 @@
+use ::bevy::prelude::*;
+
+#[derive(Component, Debug)]
+pub struct Velocity {
+    pub value: Vec3,
+}
+
+impl Velocity {
+    pub fn new(value: Vec3) -> Self {
+        Self { value }
+    }
+}
+
+#[derive(Component, Debug)]
+pub struct Acceleration {
+    pub value: Vec3,
+}
+
+impl Acceleration {
+    pub fn new(value: Vec3) -> Self {
+        Self { value }
+    }
+}
+
+pub struct MovementPlugin;
+
+impl Plugin for MovementPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Update, update_position);
+    }
+}
+
+fn update_position(mut query: Query<(&Velocity, &mut Transform)>, time: Res<Time>) {
+    for (velocity, mut transform) in query.iter_mut() {
+        transform.translation += velocity.value * time.delta_seconds();
+    }
+}
